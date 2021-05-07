eec51d51666c89e7358b11860d781e8a25d2a83f
phaeton <542068262@qq.com>
2021-05-08 00:16:49 +0800

    httpServer简单的代码，没有dispatcher和业务代码

---
+++ netty-http/src/main/java/pojo/User.java
@@ -0,0 +1,14 @@
+package pojo;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class User {
+    private String userName;
+
+    private String method;
+
+    private Date date;
+}
